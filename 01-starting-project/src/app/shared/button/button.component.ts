5a1f5640453557839cbdcd5b2430f83241248bda
ibrahim-bayramov <ibrahim-bayramov@hotmail.com>
2024-10-01 05:26:26 +0200

    ng content instead of @input

--- 01-starting-project/src/app/shared/button/button.component.ts
+++ 01-starting-project/src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[app-button]',
+  selector: 'button[app-button],a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
